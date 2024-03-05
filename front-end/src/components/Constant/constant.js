2b7dce0adbc4ff7aeebf93a1b1f0f389be4a0514
amaljithmk07 <amaljithmk123@gmail.com>
2024-03-05 10:02:45 +0530

    update constant

--- front-end/src/components/Constant/constant.js
+++ front-end/src/components/Constant/constant.js
@@ -1,4 +1,4 @@
-// const Base_URL = `https://med-equip.onrender.com`;
-const Base_URL = `http://localhost:2222`;
+const Base_URL = `https://med-equip.onrender.com`;
+// const Base_URL = `http://localhost:2222`;
 
 module.exports = Base_URL;
