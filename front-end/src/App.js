8235b384d306efe72743afe5dd14fa958b74af86
amaljithmk07 <amaljithmk123@gmail.com>
2024-03-06 10:02:26 +0530

    added exact

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -43,7 +43,7 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} exact />
         <Route path="/home" element={<Home />} />
 
         <Route path="/register" element={<RegistrationPage />} />
